3658444782cdceea87c117fde580bd2c7f02b155
David Shek <ditreyw@gmail.com>
2026-04-22 12:07:49 +0800

    fix: static

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,15 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link rel='shortcut icon' href='/favicon.ico' />
-          <link rel='icon' type='image/png' sizes='32x32' href='favicon.png' />
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <meta name="theme-color" content="#ffffff" />
 
-          <link rel='manifest' href='/manifest.json' />
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
         </Head>
 
         <body>
